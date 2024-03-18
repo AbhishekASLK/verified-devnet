092a25aaa19bc018480170958cd92a9827acb605
AbhishekASLK <bhoslea925@gmail.com>
2024-03-18 16:43:30 +0530

    splash-screen

--- lib/starter/splashscreen.dart
+++ lib/starter/splashscreen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:verified_devnet/starter/askmenu.dart';
 
@@ -15,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      const Duration(seconds: 1),
+      const Duration(seconds: 5),
       () {
         Navigator.pushReplacement(
           context,
@@ -33,21 +32,31 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(33, 17, 52, 1),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const SizedBox(
-            height: 90,
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(55, 40, 74, 1),
+              Color.fromRGBO(194, 23, 191, 1),
+              Color.fromRGBO(55, 40, 74, 1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.asset("assets/images/name.png"),
-          ),
-          const Spacer(),
-          Image.asset(
-            "assets/images/splashimage.png",
-          ),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset("assets/images/appName.png"),
+            ),
+          ],
+        ),
       ),
     );
   }
